b5408be8c9a080bde9469579b8a6489e38a2e504
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-05-17 16:35:21 +0200

    Gestion des urls incorrectes sur la page "Article".

--- star-wars-blog/src/pages/Article.jsx
+++ star-wars-blog/src/pages/Article.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import '../styles/index.css'
 import '../styles/Article.css'
 import Datas from '../datas/LocalApi.json'
@@ -11,43 +11,61 @@ import { Link } from 'react-router-dom'
 export default function Article() {
 
     const [item, setItem] = useState()
+    const [rightPage, updateRightPage] = useState(false)
+    const navigate = useNavigate()
     const { paramsIds } = useParams()
     const currentIds = paramsIds.split('.')
     const categoryId = currentIds[0]
     const articleId = currentIds[1]
 
-    const currentCategory = Datas.find((item) => item._id === categoryId)
-    const currentKeyword = currentCategory.keyword
+    const currentDatas = Datas.find((item) => item._id === categoryId)
+
+    
 
     useEffect(() => {
-        fetch(`https://starwars-databank-server.vercel.app/api/v1/${currentKeyword}/${articleId}`)
-        .then(response => response.json())
-        .then(data => setItem(data))
-        .catch((error) => console.log(error))
-    }, [currentKeyword, articleId])
+        if (currentDatas !== undefined) {
+            // Récupération de l'article depuis l'API
+            fetch(`https://starwars-databank-server.vercel.app/api/v1/${currentDatas.keyword}/${articleId}`)
+            .then(response => response.json())
+            .then(data => {
+                updateRightPage(true)
+                setItem(data)
+            })
+            .catch(error => {
+                navigate("*")
+            })
+        } else {
+            navigate("*")
+        }
+    }, [articleId, currentDatas, navigate])
+
 
     return (
-        <div className='app'>
-            <div className='div-return'>
-                <Link to={`/category/${categoryId}`} className='arrow-link'><img src={ReturnArrow} alt="Return to the last page" /></Link>
-            </div>
-            <div className='presentation'>
-                {item && (
-                    <>
-                    <div className='main-div'>
-                        <h1>{item.name.toLowerCase()}</h1>
-                        <div className='content'>
-                            <div className='img-presentation'>
-                                <img src={item.image} alt={item.name} />
-                            </div>
-                            <div className='description-div'>
-                                <p>{item.description}</p>
-                            </div>
-                        </div>
+        <>
+            {rightPage ? (
+                <div className='app'>
+                    <div className='div-return'>
+                        <Link to={`/category/${categoryId}`} className='arrow-link'><img src={ReturnArrow} alt="Return to the last page" /></Link>
                     </div>
-                    </>
-                )}
-            </div>
-        </div>
+                    <div className='presentation'>
+                        {item && (
+                            <>
+                            <div className='main-div'>
+                                <h1>{item.name.toLowerCase()}</h1>
+                                <div className='content'>
+                                    <div className='img-presentation'>
+                                        <img src={item.image} alt={item.name} />
+                                    </div>
+                                    <div className='description-div'>
+                                        <p>{item.description}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            </>
+                        )}
+                    </div>
+                </div>
+            ) : null}
+        </>
     )
 }
