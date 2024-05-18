3bbde41febd2f5cdec53ba4560a10900c269f87e
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-05-18 19:22:01 +0200

    Refonte de la structure et des styles de la page "Error404".

--- star-wars-blog/src/pages/Error404.jsx
+++ star-wars-blog/src/pages/Error404.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Error404Image from '../assets/images/error404.webp'
 import '../styles/index.css'
 import '../styles/Error404.css'
 
 export default function Error404() {
   return (
-    <div className='app'>
-      <div className='error404-div'>
-        <img src={Error404Image} alt="Obiwan influences a weak mind with the Force." />
-        <div className='overlay' />
+    <div className='app error-page'>
+      <div className='error-overlay' />
+      <div className='error404-div'>        
         <h1>Ce ne sont pas ces droïdes-là que vous recherchez...</h1>
         <Link to='/' >Retour à la page d'Accueil</Link>
       </div>
