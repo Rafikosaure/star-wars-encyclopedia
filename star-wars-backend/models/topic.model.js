d80b222708c2d67fe050688030be22cbfa2a32b9
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-06-05 18:16:39 +0200

    Ajout de la categorie dans le modèle "Topic".

--- star-wars-backend/models/topic.model.js
+++ star-wars-backend/models/topic.model.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 
 const topicSchema = new mongoose.Schema({
+    category: { type: String, required: true },
     name: { type: String, required: true },
     description: { type: String, required: true },
     posts: [
