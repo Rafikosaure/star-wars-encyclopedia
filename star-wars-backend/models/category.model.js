1c36754433450c5a61925604002b4de13ed29033
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-06-07 23:22:52 +0200

    Création du modèle "Category" pour les sujets de discussion.

---
+++ star-wars-backend/models/category.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+
+const categorySchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    topics: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Topic"
+        }
+    ]
+});
+
+module.exports = mongoose.model("Category", categorySchema);
--- star-wars-backend/models/topic.model.js
+++ star-wars-backend/models/topic.model.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 
 const topicSchema = new mongoose.Schema({
-    category: { type: String, required: true },
     name: { type: String, required: true },
     description: { type: String, required: true },
     posts: [
