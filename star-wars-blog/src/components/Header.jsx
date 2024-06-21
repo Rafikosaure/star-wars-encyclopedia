1db74335fa7de1c0ce41127c562a64033428a2ae
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-06-21 13:43:58 +0200

    Débuggage de l'affichage d'une photo de profil incorrecte après une déconnexion suivie d'une connexion d'un utilisateur différent.

--- star-wars-blog/src/components/Header.jsx
+++ star-wars-blog/src/components/Header.jsx
@@ -57,6 +57,7 @@ export default function Header() {
     .then(data => {
       // console.log(data)
       dispatch(updateLoggedUser(false))
+      dispatch(updateLoadedUser(false))
     })
     .catch(error => console.log(error))
   }
