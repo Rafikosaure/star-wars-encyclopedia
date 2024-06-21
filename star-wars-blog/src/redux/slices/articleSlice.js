31170960aaca98c09252578907bece6b5b87a6c7
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-06-21 17:07:04 +0200

    Rapide test sur l'update de l'image d'un utilisateur, et clean du code dans le state global "article".

--- star-wars-blog/src/redux/slices/articleSlice.js
+++ star-wars-blog/src/redux/slices/articleSlice.js
@@ -8,7 +8,6 @@ export const articleSlice = createSlice({
   reducers: {
     saveAnArticle: (state, action) => {
         state.value = action.payload
-        console.log(state.value)
     }
   }
 });
