20ff5475dd547bc9a0662bc22ff0428989bafb71
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-06-30 02:02:38 +0200

    Correction d'un bug graphique sur le compteur de dizaine d'articles (page Category).

--- star-wars-blog/src/pages/Category.jsx
+++ star-wars-blog/src/pages/Category.jsx
@@ -90,7 +90,9 @@ export default function Category() {
                 )
               }
             </div>
-            <div className='dozen-indicator' style={{display: article.value ? 'none' : 'block'}}>{`${storedDozen} / ${nbDozen.current}`}</div>
+            <div className='dozen-indicator' style={{display: article.value ? 'none' : 'block'}}>
+              {nbDozen.current && storedDozen && (`${storedDozen} / ${nbDozen.current}`)}
+            </div>
         </div>
         <div className='next-arrow-section' 
           style={{display: storedDozen >= nbDozen.current || article.value ? 'none' : 'flex'}} 
