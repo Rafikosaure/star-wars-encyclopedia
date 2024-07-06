3566fadb7a63afc78a0dd4ea8a19da3389e3abff
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-07-06 20:52:16 +0200

    Création du composant UserData pour la page Account en mode admin.

---
+++ star-wars-blog/src/components/UserData.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import DefaultAvatar from '../assets/images/EmojiBlitzBobaFett1.webp'
+import Delete from '../assets/images/delete.webp'
+import '../styles/UserData.css'
+
+export default function UserData({ user }) {
+
+  return (
+    <div className='user-data-table'>
+        <div className='user-data-picture'>
+            {user.picture !== "" ? (
+                <img src={user.picture} alt={user.name} />
+            ) : (
+                <img src={DefaultAvatar} alt='avatar par défaut' />
+            )}
+        </div>
+        <div className='user-data-name'>{user.name}</div>
+        <div className='user-data-delete-button'><img className='user-data-delete-image' src={Delete} alt='croix de suppression' /></div>
+    </div>
+  )
+}
