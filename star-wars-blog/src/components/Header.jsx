091547d10760a1dff5401ead6cad7966891a8bc5
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-07-06 20:51:21 +0200

    Correction d'un bug d'affichage des images de profil après la suppression d'un utilisateur.

--- star-wars-blog/src/components/Header.jsx
+++ star-wars-blog/src/components/Header.jsx
@@ -39,6 +39,7 @@ export default function Header() {
       .catch(error => {
         // console.log(error)
         dispatch(updateLoggedUser(false))
+        setLoggedUser()
       })
     }
     
