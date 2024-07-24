cb24a833f06a960bc2807bb363b5155c7cff5770
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-07-24 16:42:50 +0200

    Conception du composant TopicCard.

--- star-wars-blog/src/components/TopicCard.jsx
+++ star-wars-blog/src/components/TopicCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../styles/TopicCard.css'
 import { Link } from 'react-router-dom'
+import StarsSky from '../assets/images/ciel_nuit_banniere.webp'
 
 
 export default function TopicCard({ topic }) {
     return (
         <div className='topic-card-main'>
+            <img className='topic-card-banner' src={StarsSky} alt="Ciel étoilé" />
             <Link className='topic-card-link'>
                 <div className='topic-card-content'>
                     <h1>{topic.title}</h1>
