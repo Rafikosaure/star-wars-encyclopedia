6a027b71b98d14d7285880a121402e78767c6a36
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-07-25 13:48:22 +0200

    Création et première implémentation de la page "Topic" (au singulier) où se trouve la discussion sélectionnée.

---
+++ star-wars-blog/src/pages/Topic.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../styles/Topic.css'
+
+
+export default function Topic() {
+
+    return (
+        <div className='app topic'>
+            {/* <div className='topics-overlay' />
+                <div className='topics-main'>
+                    {currentData.title && (
+                    <h1 className='topics-page-title'>{currentData.title.toLowerCase()}</h1>
+                    )}
+                <div className='topics-list'>
+                    {currentData.topics && 
+                        (currentData.topics.map((topic, index) => 
+                            <TopicCard key={index} topic={topic} />
+                        ))
+                    }
+                </div>
+            </div>       */}
+        </div>
+    )
+}
