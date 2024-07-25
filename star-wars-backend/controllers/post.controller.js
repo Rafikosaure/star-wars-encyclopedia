a1b66712742577839382750ead11cb85291d5902
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-07-25 13:45:09 +0200

    Mise en place du contrôleur pour la route de récupération des posts par l'id du topic.

--- star-wars-backend/controllers/post.controller.js
+++ star-wars-backend/controllers/post.controller.js
@@ -5,16 +5,20 @@ require('dotenv').config()
 
 exports.createPost = async (req, res) => {
 
+    // Récupérer l'id du topic courant
     const topicId = req.params.id
 
+    // Tenter de trouver le topic courant par son id
     const currentTopic = await Topic.findById(topicId)
     if (!currentTopic) res.status(404).json({
         message: "Topic not found!"
     })
 
+    // Attribution du titre du topic courant au post
     let reqPost = req.body
     reqPost.title = currentTopic.title
 
+    // Création du post et enregistrement dans le topic courant
     Post.create(reqPost)
     .then(newPost => {
         newPost.save()
@@ -23,7 +27,33 @@ exports.createPost = async (req, res) => {
         res.status(201).json({
             newPost
         })})
+    // Gestion de l'échec potentielle de la procédure
     .catch(() => res.status(400).json({
         message: "Echec de la création du post !"
     }))
+}
+
+
+exports.getPostsByTopicId = async (req, res) => {
+    
+    // Récupérer l'id du topic courant
+    const topicId = req.params.id
+
+    // Vérifier que l'id n'est pas undefind
+    if (!topicId) res.status(404).json({
+        message: "Identifiant incorrect !"
+    })
+
+    // Trouver les posts liés à ce topic
+    const currentTopicWithPosts = await Topic.findById(topicId).populate("posts")
+
+    // Vérifier que les données ne sont pas undefined
+    if (!currentTopicWithPosts) res.status(404).json({
+        message: "Topic not found!"
+    })
+    
+    // Envoyer le topic ainsi que le tableau des posts
+    res.status(200).json(
+        currentTopicWithPosts
+    )
 }
