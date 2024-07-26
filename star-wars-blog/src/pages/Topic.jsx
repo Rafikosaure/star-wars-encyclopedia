109c0a74d43e05f3eda2272887d476852db332fa
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-07-26 12:21:19 +0200

    Implémentation nouvelle de la page "Topic" (au singulier) avec ses styles.

--- star-wars-blog/src/pages/Topic.jsx
+++ star-wars-blog/src/pages/Topic.jsx
@@ -1,24 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/Topic.css'
+import { useSelector } from 'react-redux'
+import { selectTopicsData } from '../redux/slices/topicSlice'
+import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import PostCard from '../components/PostCard'
+import { toast } from 'sonner'
 
 
 export default function Topic() {
 
+    const topicsData = useSelector(selectTopicsData)
+    const { topicId } = useParams()
+    const [currentTopicData, setCurrentTopicData] = useState()
+    const navigate = useNavigate()
+    
+    useEffect(() => {
+        if (topicsData) {
+            const verifyId = topicsData.find((item) => item._id === topicId)
+            if (!verifyId) {
+                navigate("/forum")
+                toast("Page inexistante !")
+            }
+            setCurrentTopicData(topicsData.find((topicData) => topicData._id === topicId))
+        }
+    }, [topicId, topicsData, navigate])
+
+
+
     return (
         <div className='app topic'>
-            {/* <div className='topics-overlay' />
-                <div className='topics-main'>
-                    {currentData.title && (
-                    <h1 className='topics-page-title'>{currentData.title.toLowerCase()}</h1>
+            <div className='topic-overlay' />
+            {topicsData && (
+                <div className='topic-content'>
+                    <h1 className='topic-title'>Topic-title</h1>
+                    {currentTopicData && (
+                        <div className='topic-list'>
+                            {currentTopicData.posts.map((post, index) => (
+                                <PostCard key={index} index={index} post={post}/>
+                            ))}
+                        </div>
                     )}
-                <div className='topics-list'>
-                    {currentData.topics && 
-                        (currentData.topics.map((topic, index) => 
-                            <TopicCard key={index} topic={topic} />
-                        ))
-                    }
                 </div>
-            </div>       */}
+            )}
         </div>
     )
 }
