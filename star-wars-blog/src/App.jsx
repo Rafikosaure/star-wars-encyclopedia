e6f181f7b503ab8685c643e4f3b5e22ca65001b6
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-07-26 16:33:10 +0200

    Retour de la page laboratoire dans "App.jsx".

--- star-wars-blog/src/App.jsx
+++ star-wars-blog/src/App.jsx
@@ -10,6 +10,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Topic from './pages/Topic'
 import Account from './pages/Account';
+import TestPage from './pages/TestPage';
 
 
 export default function App() {
@@ -26,6 +27,7 @@ export default function App() {
               <Route path='/topics/:topicsCategoryId' element={<Topics />} />
               <Route path='/account' element={<Account />} />
               <Route path='/topic/:topicId' element={<Topic />} />
+              <Route path='/testPage' element={<TestPage />} />
               <Route path='*' element={<Error404 />} />
           </Routes>
           <Footer />
