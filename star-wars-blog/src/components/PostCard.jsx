f52ec670176be78be62cb9483f2580f364a59fab
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-07-29 15:06:32 +0200

    Travail de conception du composant PostCard, non-terminé.

--- star-wars-blog/src/components/PostCard.jsx
+++ star-wars-blog/src/components/PostCard.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import '../styles/PostCard.css'
 import DefaultAvatar from '../assets/images/EmojiBlitzBobaFett1.webp'
+import Like from './Like'
 
 
 
 export default function PostCard({ index, post }) {
 
     const [postUser, setPostUser] = useState()
+    const [datetime, updateDateTime] = useState()
 
     useEffect(() => {
         if (post) {
@@ -16,6 +18,8 @@ export default function PostCard({ index, post }) {
                 // console.log(data)
                 if (!data.message) {
                     setPostUser(data)
+                    const myDate = new Date(post.createdAt)
+                    updateDateTime(myDate)
                 }
             })
             .catch(error => console.log(error))
@@ -27,7 +31,12 @@ export default function PostCard({ index, post }) {
         <div className='post-card-main'>
             <div className='post-card-overlay' />
             <div className='post-card-content'>
-                <p>{`# ${index + 1}`}</p>
+                {datetime && (
+                    <div className='post-card-infos'>
+                        <p>{`# ${index + 1}`}</p><p className='infos-datetime'>{`Le ${datetime.getDate()}/${datetime.getMonth() + 1}/${datetime.getFullYear()} à ${datetime.getHours() + 1}h${datetime.getUTCMinutes()}`}</p>
+                    </div>
+                )}
+                
                 <div className='post-card-author'>
                     {postUser ? (
                         <div className='post-card-author-wrapper'>
@@ -45,10 +54,13 @@ export default function PostCard({ index, post }) {
                         </div>
                     )}
                     <div className='post-card-message'>
-                    {post && (
-                        <p>{post.content}</p>
-                    )}
+                        {post && (
+                            <p>{post.content}</p>
+                        )}
+                    </div>
                 </div>
+                <div className='post-card-bottom-bar'>
+                    <Like />
                 </div>
             </div>
             
