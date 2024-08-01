7a0feed1fa82c680764e8a1680b1f07b967b2f7d
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-08-01 22:16:36 +0200

    Ajout de la date de création du topic dans le composant TopicCard, avec reprise du style du composant.

--- star-wars-blog/src/components/TopicCard.jsx
+++ star-wars-blog/src/components/TopicCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import '../styles/TopicCard.css'
 import { Link } from 'react-router-dom'
 import StarsSky from '../assets/images/ciel_nuit_banniere.webp'
@@ -7,13 +8,25 @@ import StarsSky from '../assets/images/ciel_nuit_banniere.webp'
 
 export default function TopicCard({ topic }) {
 
+    const [datetime, updateDateTime] = useState()
+
+    useEffect(() => {
+        const topicDate = new Date(topic.createdAt)
+        updateDateTime(topicDate)
+    }, [topic])
+
+
     return (
         <div className='topic-card-main'>
             <img className='topic-card-banner' src={StarsSky} alt="Ciel étoilé" />
             <Link className='topic-card-link' to={`/topic/${topic._id}`}>
                 <div className='topic-card-content'>
-                    <h1>{topic.title}</h1>
-                    <p>{topic.description}</p>
+                    {topic && datetime && (
+                        <>
+                            <div className='topic-infos'><h1>{topic.title}</h1><p>{`Créé le ${datetime.getDate()}/${datetime.getMonth() + 1}/${datetime.getFullYear()} à ${datetime.getHours() + 1}h${datetime.getUTCMinutes()}`}</p></div>
+                            <p>{topic.description}</p>
+                        </>
+                    )}
                 </div>
             </Link>
         </div>
