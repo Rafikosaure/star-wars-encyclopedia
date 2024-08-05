2e4a618cc5ab95658bb91b66683a31112ad02a7c
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-08-05 11:54:43 +0200

    Mise en place des routes pour l'utilisation des likes sur les posts.

--- star-wars-backend/routes/like.routes.js
+++ star-wars-backend/routes/like.routes.js
@@ -5,14 +5,8 @@ const router = express.Router()
 
 
 /** Routes de gestion des likes des utilisateurs sur les posts */
+router.post('/attributeLike/:id', auth.verifyToken, likeCtrl.attributeLike)
+router.delete('/dislike/:id', auth.verifyToken, likeCtrl.dislike)
 
-// router.post('/register', multer, userCtrl.register)
-// router.post('/login', userCtrl.login)
-// router.post('/logout', auth.verifyToken, userCtrl.logout)
-// router.get('/logged', auth.verifyToken, userCtrl.logged)
-// router.get('/getAll', auth.verifyToken, userCtrl.getAllUsers)
-// router.put('/update', auth.verifyToken, multer, userCtrl.modifyUser)
-// router.delete('/authDeleteById/:id', auth.verifyToken, userCtrl.authDeleteById)
-// router.delete('/deleteById', auth.verifyToken, userCtrl.deleteById)
 
 module.exports = router
