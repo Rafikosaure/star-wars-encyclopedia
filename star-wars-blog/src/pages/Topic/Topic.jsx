79b1bba953175cf28ea9f428ea89773134ea1c07
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-08-14 18:30:16 +0200

    Mise en place de la recherche des posts par id de topic au sein de la page Topic, avec gestion des cas d'erreur.

--- star-wars-blog/src/pages/Topic/Topic.jsx
+++ star-wars-blog/src/pages/Topic/Topic.jsx
@@ -1,41 +1,59 @@
 import React, { useEffect, useState } from 'react'
 import './Topic.scss'
-import { useSelector } from 'react-redux'
-import { selectTopicsData } from '../../redux/slices/topicSlice'
 import { useParams } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import PostCard from '../../components/PostCard/PostCard'
 import { Link } from 'react-router-dom'
 import ReturnArrow from '../../assets/images/return-arrow.webp'
+import config from '../../config'
 
 
 export default function Topic() {
-
-    const topicsData = useSelector(selectTopicsData)
+    
+    
     const { topicId } = useParams()
     const [currentTopicData, setCurrentTopicData] = useState()
+    const [currentCategory, setCurrentCategory] = useState()
     const navigate = useNavigate()
-    
+
+
     useEffect(() => {
-        if (topicsData) {
-            const verifyId = topicsData.find((item) => item._id === topicId)
-            if (!verifyId) {
+        fetch(`${config.serverEndpoint}/post/getPostsByTopicId/${topicId}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.error) {
                 navigate("*")
+            } else {
+                setCurrentTopicData(data)
             }
-            setCurrentTopicData(topicsData.find((topicData) => topicData._id === topicId))
-        }
-    }, [topicId, topicsData, navigate])
+        })
+        .catch(error => console.log(error))
+    }, [navigate, topicId])
+
+
+    useEffect(() => {
+        fetch(`${config.serverEndpoint}/category/findCategoryFromTopic/${topicId}`)
+        .then(response => response.json())
+        .then(data => {
+            setCurrentCategory(data.category[0])
+        })
+        .catch(error => console.log(error))
+
+    }, [topicId])
 
 
 
     return (
         <div className='app topic'>
             <div className='topic-overlay' />
-            <div className='topic-div-return' title='Retour vers les topics'>
-                <Link to={-1} className='topic-arrow-link'>
-                    <img src={ReturnArrow} alt="Return to the topics page" />
-                </Link>
-            </div>
+            {currentCategory && (
+                <div className='topic-div-return' title='Retour vers les topics'>
+                    <Link to={`/topics/${currentCategory._id}`} className='topic-arrow-link'>
+                        <img src={ReturnArrow} alt="Return to the topics page" />
+                    </Link>
+                </div>
+            )}
+            
             {currentTopicData && (
                 <div className='topic-content'>
                     <h1 className='topic-title'>{currentTopicData.title}</h1>
