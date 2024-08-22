1314b7860dd1a0383b2b6e86c370488de18e285c
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-08-22 21:48:01 +0200

    Retrait du contrôleur de récupération des topics avec leurs posts associés -> devenu inutile.

--- star-wars-backend/controllers/topic.controller.js
+++ star-wars-backend/controllers/topic.controller.js
@@ -4,19 +4,19 @@ const Post = require('../models/post.model.js')
 
 
 
-exports.getTopicsAndPosts = async (req, res) => {
+// exports.getTopicsAndPosts = async (req, res) => {
 
-    // Chercher tous les topics avec leurs posts
-    const topicsWithPosts = await Topic.find().populate('posts')
+//     // Chercher tous les topics avec leurs posts
+//     const topicsWithPosts = await Topic.find().populate('posts')
 
-    // Vérifier si les données sont valides
-    if (!topicsWithPosts) res.status(404).json({
-        message: "Topics not found!"
-    })
+//     // Vérifier si les données sont valides
+//     if (!topicsWithPosts) res.status(404).json({
+//         message: "Topics not found!"
+//     })
 
-    // Renvoyer l'objet récupéré en réponse
-    res.status(200).json(topicsWithPosts)
-}
+//     // Renvoyer l'objet récupéré en réponse
+//     res.status(200).json(topicsWithPosts)
+// }
 
 exports.getTopics = (req, res) => {
     Topic.find()
@@ -62,43 +62,49 @@ exports.getTopicsByCategory = async (req, res) => {
 
 
 exports.createTopic = async (req, res) => {
+    try {
+        // Récupérer l'id de la catégorie courante
+        const categoryId = req.params.id
 
-    // Récupérer l'id de la catégorie courante
-    const categoryId = req.params.id
+        // Avec cet id, récupérer la catégorie courante elle-même
+        const currentCategory = await Category.findById(categoryId)
+        if (!currentCategory) res.status(404).json({
+            message: "Category not found!"
+        })
 
-    // Avec cet id, récupérer la catégorie courante elle-même
-    const currentCategory = await Category.findById(categoryId)
-    if (!currentCategory) res.status(404).json({
-        message: "Category not found!"
-    })
+        // Créer le topic et l'insérer dans la catégorie courante
+        const newTopic = await Topic.create(req.body.topic)
+        if (newTopic) {
+            newTopic.save()
+            currentCategory.topics.push(newTopic)
+            currentCategory.save()
+        } else {
+            res.status(500).json({
+                message: "Topic creation failed!"
+            })
+        }
 
-    // Créer le topic et l'insérer dans la catégorie courante
-    const newTopic = await Topic.create(req.body.topic)
-    if (newTopic) {
-        newTopic.save()
-        currentCategory.topics.push(newTopic)
-        currentCategory.save()
-    } else {
+        // Créer un premier post et l'insérer dans le nouveau topic
+        let reqPost = req.body.post
+        reqPost.title = newTopic.title
+        const newPost = await Post.create(reqPost)
+        if (newPost) {
+            newPost.save()
+            newTopic.posts.push(newPost)
+            newTopic.save()
+            res.status(201).json({
+                topic: newTopic,
+                post: newPost
+            })
+        } else {
+            res.status(400).json({
+                message: "Post creation failed!"
+            })
+        }
+    } catch(error) {
         res.status(500).json({
-            message: "Echec de la création du topic !"
+            message: error
         })
     }
-
-    // Créer un premier post et l'insérer dans le nouveau topic
-    let reqPost = req.body.post
-    reqPost.title = newTopic.title
-    const newPost = await Post.create(reqPost)
-    if (newPost) {
-        newPost.save()
-        newTopic.posts.push(newPost)
-        newTopic.save()
-        res.status(201).json({
-            topic: newTopic,
-            post: newPost
-        })
-    } else {
-        res.status(400).json({
-            message: "Echec de la création du post !"
-        })
-    }
+    
 }
