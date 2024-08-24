4df47844c7d435aee083c0c3461297056185bec4
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-08-24 10:28:50 +0200

    Clean du code du modèle "Like".

--- star-wars-backend/models/like.model.js
+++ star-wars-backend/models/like.model.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 
 const likeSchema = new mongoose.Schema({
-    // likeType: { type: mongoose.Schema.Types.ObjectId, enum: ["Post", "Comment"], require: true },
     post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", require: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", require: true },
     createdAt: { type: Date, default: Date.now }
