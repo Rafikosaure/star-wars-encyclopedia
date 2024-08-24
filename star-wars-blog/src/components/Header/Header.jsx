976f29d2f4fb7d360d985c7aafb6b472d2969899
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-08-24 12:00:55 +0200

    MIse en place des styles "isActive" des Navlink dans le composant Header.

--- star-wars-blog/src/components/Header/Header.jsx
+++ star-wars-blog/src/components/Header/Header.jsx
@@ -93,14 +93,46 @@ export default function Header() {
         <div className='title-and-links'>
           <h1>Star Wars Encyclopedia</h1>
           <nav>
-            <NavLink to="/category/f6def03b-7818-4951-9d8a-afbe17aa205b" onClick={() => dispatch(reinitializeDozen())}>personnages</NavLink>
-            <NavLink to="/category/645e063c-af40-4576-b674-d44be61b0773" onClick={() => dispatch(reinitializeDozen())}>créatures</NavLink>
-            <NavLink to="/category/2857b650-12ec-4671-86a8-0d5c5db0518e" onClick={() => dispatch(reinitializeDozen())}>droïds</NavLink>
-            <NavLink to="/category/a5ebca47-0ce4-452c-84c2-2d1c23309458" onClick={() => dispatch(reinitializeDozen())}>lieux</NavLink>
-            <NavLink to="/category/13043736-7457-4a22-9218-4f9134f61b0c" onClick={() => dispatch(reinitializeDozen())}>organisations</NavLink>
-            <NavLink to="/category/d4ac0725-f089-42c4-a76b-817764c5e0ab" onClick={() => dispatch(reinitializeDozen())}>espèces</NavLink>
-            <NavLink to="/category/1812c326-e3a3-4a84-9a69-4dd64f5a298d" onClick={() => dispatch(reinitializeDozen())}>véhicules</NavLink>
-            <NavLink to="/forum" onClick={() => dispatch(reinitializeDozen())}>Forum</NavLink>
+            <NavLink className={({ isActive }) =>
+                        isActive
+                        ? 'navlink-active'
+                        : 'navlink-inactive'
+                        } to="/category/f6def03b-7818-4951-9d8a-afbe17aa205b" onClick={() => dispatch(reinitializeDozen())}>personnages</NavLink>
+            <NavLink className={({ isActive }) =>
+                        isActive
+                        ? 'navlink-active'
+                        : 'navlink-inactive'
+                        } to="/category/645e063c-af40-4576-b674-d44be61b0773" onClick={() => dispatch(reinitializeDozen())}>créatures</NavLink>
+            <NavLink className={({ isActive }) =>
+                        isActive
+                        ? 'navlink-active'
+                        : 'navlink-inactive'
+                        } to="/category/2857b650-12ec-4671-86a8-0d5c5db0518e" onClick={() => dispatch(reinitializeDozen())}>droïds</NavLink>
+            <NavLink className={({ isActive }) =>
+                        isActive
+                        ? 'navlink-active'
+                        : 'navlink-inactive'
+                        } to="/category/a5ebca47-0ce4-452c-84c2-2d1c23309458" onClick={() => dispatch(reinitializeDozen())}>lieux</NavLink>
+            <NavLink className={({ isActive }) =>
+                        isActive
+                        ? 'navlink-active'
+                        : 'navlink-inactive'
+                        } to="/category/13043736-7457-4a22-9218-4f9134f61b0c" onClick={() => dispatch(reinitializeDozen())}>organisations</NavLink>
+            <NavLink className={({ isActive }) =>
+                        isActive
+                        ? 'navlink-active'
+                        : 'navlink-inactive'
+                        } to="/category/d4ac0725-f089-42c4-a76b-817764c5e0ab" onClick={() => dispatch(reinitializeDozen())}>espèces</NavLink>
+            <NavLink className={({ isActive }) =>
+                        isActive
+                        ? 'navlink-active'
+                        : 'navlink-inactive'
+                        } to="/category/1812c326-e3a3-4a84-9a69-4dd64f5a298d" onClick={() => dispatch(reinitializeDozen())}>véhicules</NavLink>
+            <NavLink className={({ isActive }) =>
+                        isActive
+                        ? 'navlink-active'
+                        : 'navlink-inactive'
+                        } to="/forum" onClick={() => dispatch(reinitializeDozen())}>Forum</NavLink>
           </nav>
           <div className='header-div-connection'>
             {!isLogged ? (
