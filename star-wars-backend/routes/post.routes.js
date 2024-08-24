9ba29ce7a8f13a8572ef2b23ee6dfd07439080ca
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-08-24 10:27:45 +0200

    Création de la route pour supprimer un post.

--- star-wars-backend/routes/post.routes.js
+++ star-wars-backend/routes/post.routes.js
@@ -7,12 +7,9 @@ const router = express.Router()
 /** Routes de création et de gestion des posts dans le forum */
 
 router.post('/createPost/:id', auth.verifyToken, postCtrl.createPost)
-// router.post('/login', userCtrl.login)
-// router.post('/logout', auth.verifyToken, userCtrl.logout)
 router.get('/getPostsByTopicId/:id', postCtrl.getPostsByTopicId)
 router.get('/getPostAuthor/:id', postCtrl.getPostAuthor)
 // router.put('/update', auth.verifyToken, multer, userCtrl.modifyUser)
-// router.delete('/authDeleteById/:id', auth.verifyToken, userCtrl.authDeleteById)
-// router.delete('/deleteById', auth.verifyToken, userCtrl.deleteById)
+router.delete('/deletePostById/:id', auth.verifyToken, postCtrl.deletePostById)
 
 module.exports = router
