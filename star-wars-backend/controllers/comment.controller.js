688e595c46ecb6c9d43195d1dec76989d51d062f
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-08-27 23:14:16 +0200

    Correction rapide des messages du contrôleur de création de commentaire : passage vers l'anglais.

--- star-wars-backend/controllers/comment.controller.js
+++ star-wars-backend/controllers/comment.controller.js
@@ -4,6 +4,6 @@ const Comment = require('../models/comment.model.js')
 exports.createComment = (req, res) => {
     const comment = new Comment(req.body)
     comment.save()
-        .then(() => res.status(201).json({ message: 'Message créé !' }))
-        .catch(() => res.status(400).json({ message: 'Echec de la création du message !' }))
+        .then(() => res.status(201).json({ message: 'Comment created!' }))
+        .catch(() => res.status(400).json({ message: 'Comment creation failed!' }))
 }
