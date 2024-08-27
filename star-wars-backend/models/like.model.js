8a88d8a083412a2063ce4a769dbc918ec95ae56a
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-08-27 23:12:36 +0200

    Modification de l'entrée "post" dans le modèle "Like" : possibilité grâce au mot-clé "enum" d'enregistrer des likes attribués à des posts mais aussi à des commentaires (et renommage de la clé de "post" à "likeType").

--- star-wars-backend/models/like.model.js
+++ star-wars-backend/models/like.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 const likeSchema = new mongoose.Schema({
-    post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", require: true },
+    likeType: { type: mongoose.Schema.Types.ObjectId, enum: ["Post", "Comment"], require: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", require: true },
     createdAt: { type: Date, default: Date.now }
 });
