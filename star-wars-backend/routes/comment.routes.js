da77ecef1e9ffb4f916f160f28da198847e994c3
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-08-31 18:32:54 +0200

    Elaboration des routes pour les commentaires.

--- star-wars-backend/routes/comment.routes.js
+++ star-wars-backend/routes/comment.routes.js
@@ -4,15 +4,10 @@ const auth = require('../middlewares/auth.js')
 const router = express.Router()
 
 
-/** Routes de création et de gestion globale des messages des posts */
-
-router.post('/createComment', commentCtrl.createComment)
-// router.post('/login', userCtrl.login)
-// router.post('/logout', auth.verifyToken, userCtrl.logout)
-// router.get('/logged', auth.verifyToken, userCtrl.logged)
-// router.get('/getAll', auth.verifyToken, userCtrl.getAllUsers)
-// router.put('/update', auth.verifyToken, multer, userCtrl.modifyUser)
-// router.delete('/authDeleteById/:id', auth.verifyToken, userCtrl.authDeleteById)
-// router.delete('/deleteById', auth.verifyToken, userCtrl.deleteById)
+/** Routes de création et de gestion globale des commentaires des posts */
+router.post('/createComment', auth.verifyToken, commentCtrl.createComment)
+router.get('/getCommentsByPost/:id', commentCtrl.getCommentsByPostId)
+router.put('/updateAComment/:id', auth.verifyToken, commentCtrl.updateACommentById)
+router.delete('/deleteAComment/:id', auth.verifyToken, commentCtrl.deleteACommentById)
 
 module.exports = router
