69723ada0b21b7c4ed1eb9eae7324970fe9f18d1
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-09-21 11:15:33 +0200

    Finalisation de l'implémentation des routes pour la gestion des commentaires : correction de la route pour la création d'un commentaire, création des routes pour la récupération d'un commentaire et pour la récupération de l'auteur d'un commentaire.

--- star-wars-backend/routes/comment.routes.js
+++ star-wars-backend/routes/comment.routes.js
@@ -5,8 +5,10 @@ const router = express.Router()
 
 
 /** Routes de création et de gestion globale des commentaires des posts */
-router.post('/createComment', auth.verifyToken, commentCtrl.createComment)
+router.post('/createComment/:id', auth.verifyToken, commentCtrl.createComment)
+router.get('/getOneComment/:id', commentCtrl.getOneCommentById)
 router.get('/getCommentsByPost/:id', commentCtrl.getCommentsByPostId)
+router.get('/getCommentAuthor/:id', commentCtrl.getCommentAuthorById)
 router.put('/updateAComment/:id', auth.verifyToken, commentCtrl.updateACommentById)
 router.delete('/deleteAComment/:id', auth.verifyToken, commentCtrl.deleteACommentById)
 
