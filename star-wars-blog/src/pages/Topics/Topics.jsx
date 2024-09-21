ad595240636dfd888024f1f911cfe7771a15c8fe
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-09-21 11:29:37 +0200

    Amélioration de la syntaxe du code dans la page "Topics" au niveau de la div "topics-list" (ligne 59).

--- star-wars-blog/src/pages/Topics/Topics.jsx
+++ star-wars-blog/src/pages/Topics/Topics.jsx
@@ -57,11 +57,13 @@ export default function Topics() {
           <h1 className='topics-page-title'>{categoryTitle.toLowerCase()}</h1>
         )}
         <div className='topics-list'>
-          {topicsData && 
-            (topicsData.map((topic, index) => 
+          {topicsData.length > 0 ? (
+            topicsData.map((topic, index) => 
               <TopicCard key={index} topic={topic} />
-            ))
-          }
+            )
+          ) : (
+            <p className='topics-list-empty-message'>Pas de discussion pour le moment...</p>
+          )}
           <div className='topic-creation-form'>
             <TopicForm topicsCategoryId={topicsCategoryId} />
           </div>
