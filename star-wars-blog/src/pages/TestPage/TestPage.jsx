550f2069c5b7a2579c661fa82bfd495de5bf44c2
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-09-29 16:44:06 +0200

    Conception du système de mention sur les utilisateurs du site.

--- star-wars-blog/src/pages/TestPage/TestPage.jsx
+++ star-wars-blog/src/pages/TestPage/TestPage.jsx
@@ -1,12 +1,34 @@
 import './TestPage.scss'
-import CommentCard from '../../components/CommentCard/CommentCard.jsx'
-
+// import { useState } from 'react'
 
 export default function TestPage() {
+  // const [artists, setArtists] = useState([])
+
+  const detectString = () => {
+    const textareaValue = "@Rafik est particulièrement fatigué de coder, @Jean ! Il faut qu'il se repose. N'est-ce pas, @Jean ?"
+    const users = ["rafik", "Jean", "andré"]
+    let includeUsersArray = []
+    users.forEach((user) => {
+      const isIncludeValue = textareaValue.toLowerCase().search(`@${user.toLowerCase()}`)
+      if (isIncludeValue !== -1) {
+        // console.log(`"${user}" est présent dans le texte.`)
+        includeUsersArray.push(user)
+      } else {
+        // console.log(`"${user}" est absent du texte.`)
+      }
+    })
+    console.log(includeUsersArray)
+  }
+
+  detectString()
 
   return (
     <div className='test-app'>
-      <CommentCard />
+      <form>
+        <textarea />
+        <button type='submit'>Submit</button>
+      </form>
+      
     </div>
   )
 }
