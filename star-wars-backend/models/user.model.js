dd48e73345ffa08661c289f46d03571183285de6
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-09-29 16:45:14 +0200

    Modification du modèle "User" : mise en "unique: true" de l'attribut "name" du modèle, pour éviter les problèmes de duplication de noms d'utilisateurs dans le système de mention.

--- star-wars-backend/models/user.model.js
+++ star-wars-backend/models/user.model.js
@@ -3,7 +3,7 @@ const uniqueValidator = require('mongoose-unique-validator')
 
 
 const userSchema = mongoose.Schema({
-    name: { type: String, required: true },
+    name: { type: String, required: true, unique: true },
     picture: { type: String, required: false },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
