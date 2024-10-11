e825eed4b999d18053a456e770fde9565bd9c4f1
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-10-11 20:01:16 +0200

    Création des contrôleurs des emails pour les notifications dans le forum.

---
+++ star-wars-backend/controllers/email.controller.js
@@ -0,0 +1,98 @@
+const nodemailer = require("nodemailer");
+const ENV = require("../config/config.js")
+const Email = require("../models/email.model.js")
+const User = require('../models/user.model.js')
+const Topic = require('../models/topic.model.js')
+
+
+
+exports.userNotificationEmail = async (req, res) => {
+    try {
+        // Find the topic
+        const topicId = req.params.id
+        const currentTopic = await Topic.findById(topicId)
+
+        // Find the author of the mentions
+        const authorId = req.user.id
+        const author = await User.findById(authorId)
+
+        // Get the mentionned users array
+        const users = req.body.users
+
+        // Get the id of the new post
+        const postId = req.body.postId
+
+        // Get the type of the email
+        const emailType = req.body.emailType
+
+        // Build the data object
+        let data = {
+            users: users,
+            author: author,
+            topic: currentTopic,
+            postId: postId,
+            emailType: emailType
+        }
+
+        // Create a transporter object
+        const transporter = nodemailer.createTransport({
+            host: ENV.EMAIL_HOST,
+            port: ENV.EMAIL_PORT,
+            secure: true, // use false for STARTTLS; true for SSL on port 465
+            auth: {
+                user: ENV.EMAIL_SENDER_ADDRESS,
+                pass: ENV.GMAIL_APP_PASSWORD,
+            }
+        });
+
+        // Define the email timestamp
+        const dateObject = new Date()
+        const options = {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            hour: '2-digit', 
+            minute: '2-digit'
+        }
+        const datetime = dateObject.toLocaleDateString("fr-FR", options).replace(':', 'h')
+
+        // Send emails to all mentionned users
+        data.users.forEach(async (mentionnedUser) => {
+            // Manage type of email
+            let emailTypeToSend;
+            if (data.emailType === "mention") {
+                emailTypeToSend = {
+                    subject: `${mentionnedUser.name}, on fait mention de vous !`,
+                    html: Email.emailMention(data, mentionnedUser, datetime)
+                }
+            }
+            const mailOptions = {
+                from: data.author.email,
+                to: mentionnedUser.email,
+                subject: emailTypeToSend.subject, // Subject of the email
+                html: emailTypeToSend.html, // Template of the email
+                attachments: [{
+                    filename: 'email-banner.jpg',
+                    path: './images/email-banner.jpg',
+                    cid: 'starwarsencyclopediabanner' // same value cid than in the src of the html image (in the template)
+                }]
+            }
+            transporter.sendMail(mailOptions)
+            .then(info => console.log("Email sent !", info.response))
+            .catch(error => res.status(404).json({
+                message: "Error! Email sending failed!",
+                error: error
+            }))
+        })
+
+        res.status(200).json({
+            message: "Email processing complete!"
+        })
+
+    } catch {
+        res.status(500).json({
+            message: "Email sending failed!"
+        })
+    }
+}
