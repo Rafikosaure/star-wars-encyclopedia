2fc8f2ecc951ec6cdf3eb16661459d28bb13601e
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-10-19 18:01:08 +0200

    Correction des attribut "required" des modèles "comment" et "like".

--- star-wars-backend/models/like.model.js
+++ star-wars-backend/models/like.model.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 
 const likeSchema = new mongoose.Schema({
-    likeType: { type: mongoose.Schema.Types.ObjectId, enum: ["Post", "Comment"], require: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", require: true },
+    likeType: { type: mongoose.Schema.Types.ObjectId, enum: ["Post", "Comment"], required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     createdAt: { type: Date, default: Date.now }
 });
 
