1e620128e19e0b23f42232db6b8290954919fd50
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-10-19 10:44:58 +0200

    Reprise du code du contrôleur de suppression de topic pour un code optimal et pour l'ajout de la suppression de l'objet followTopic lié au topic. Egalement : implémentation de la création d'un objet followTopic pour chaque topic nouvellement créé.

--- star-wars-backend/controllers/topic.controller.js
+++ star-wars-backend/controllers/topic.controller.js
@@ -2,9 +2,12 @@ const Topic = require('../models/topic.model.js')
 const Category = require('../models/category.model.js')
 const Post = require('../models/post.model.js')
 const Comment = require('../models/comment.model.js')
+const FollowTopic = require('../models/followTopic.model.js')
+const User = require('../models/user.model.js')
+const Like = require('../models/like.model.js')
 
 
-
+// Récupérer toutes les discussions
 exports.getTopics = (req, res) => {
     Topic.find()
         .then((topics) => res.status(200).json(topics))
@@ -13,9 +16,8 @@ exports.getTopics = (req, res) => {
         }))
 }
 
-
+// Récupérer toutes les discussions d'une catégorie
 exports.getTopicsByCategoryId = async (req, res) => {
-    
     try {
         // Récupérer l'id de la catégorie courante
         const categoryId = req.params.id
@@ -39,7 +41,7 @@ exports.getTopicsByCategoryId = async (req, res) => {
     }
 }
 
-
+// Créer une discussion
 exports.createTopic = async (req, res) => {
     try {
         // Récupérer l'id de la catégorie courante
@@ -63,6 +65,18 @@ exports.createTopic = async (req, res) => {
             })
         }
 
+        // Créer un tableau de followers pour ce topic
+        const userId = req.user.id
+        const creatorUser = await User.findById(userId)
+        const newFollowTopicObject = {
+            topicId: newTopic._id,
+            users: [
+                creatorUser
+            ]
+        }
+        const newFollowTopicArray = new FollowTopic(newFollowTopicObject)
+        newFollowTopicArray.save()
+
         // Créer un premier post et l'insérer dans le nouveau topic
         let reqPost = req.body.post
         reqPost.title = newTopic.title
@@ -85,12 +99,12 @@ exports.createTopic = async (req, res) => {
     }
 }
 
-
+// Supprimer une discussion
 exports.deleteTopicById = async (req, res) => {
     try {
         const topicId = req.params.id
 
-        // Trouver le topic (et récupérer ses posts)
+        // Trouver le topic et récupérer ses posts
         const currentTopic = await Topic.findById(topicId).populate('posts')
         if (!currentTopic) res.status(404).json({
             message: "Topic not found!"
@@ -101,61 +115,48 @@ exports.deleteTopicById = async (req, res) => {
         currentCategory[0].topics.pull({ _id: topicId })
         currentCategory[0].save()
 
+        // Trouver puis supprimer le tableau des followers du topic
+        await FollowTopic.findOneAndDelete({ topicId: topicId })
+
         // Posts du topic : d'abord, supprimer leurs likes
         const postsToDelete = currentTopic.posts;
-        postsToDelete.forEach(async (post) => {
+        postsToDelete.map(async (post) => {
             await Like.deleteMany({ likeType: post._id })
-        });
+        })
 
         // Trouver les commentaires des posts du topic
-        postsToDelete.forEach(async (post) => {
-            const comments = getCommentsOfAPost(post)
-            if (!comments) res.status(404).json({
-                message: "Comments not found!"
-            })
-            
-            // Supprimer les likes des comments
-            comments.forEach(async (comment) => {
-                const result = deleteLikesOfAComment(comment)
-                if (!result) res.status(404).json({
-                    message: "Likes deletion failed!"
-                })
-            })
-            // Supprimer les comments du post
-            await Comment.deleteMany({ post: post._id })
+        await Promise.all(
+            postsToDelete.map(async (post) => {
+                const postWithComments = await Post.findById(post._id).populate('comments')
+                const commentsToDelete = postWithComments.comments
+                
+                // Itérer sur les commentaires de chaque post
+                await Promise.all(
+                    commentsToDelete.map(async (comment) => {
 
-            // Supprimer les posts
-            await Post.findByIdAndDelete(post._id)
-        })
-        // Supprimer le topic
+                        // Supprimer les likes des commentaires
+                        await Like.deleteMany({ likeType: comment._id })
+                        
+                        // Supprimer le commentaire courant
+                        await Comment.deleteOne({ _id: comment._id })
+                    })
+                )
+            
+                // Supprimer le post courant
+                await Post.findByIdAndDelete(post._id)
+            })   
+        )
+
+        // Enfin, supprimer le topic
         await Topic.findByIdAndDelete(topicId)
+
+        res.status(200).json({
+            message: "Topic deletion is success!"
+        })
 
     } catch(error) {
         res.status(500).json({
             message: 'Topic deletion failed!'
         })
-    }
-}
-
-
-const deleteLikesOfAComment = async (comment) => {
-    try {
-        await Like.deleteMany({ liketype: comment._Id })
-        return true
-
-    } catch(error) {
-        console.log(error)
-        return false
-    }
-}
-
-const getCommentsOfAPost = async (post) => {
-    try {
-        const comments = await Comment.find({ post: post._id })
-        return comments
-
-    } catch(error) {
-        console.log(error)
-        return false
     }
 }
