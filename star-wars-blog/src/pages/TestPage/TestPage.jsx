0577d4b2c81ada855a95589ed926b9584e6b127b
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-11-09 17:17:51 +0100

    Conception du composant "FollowedTopicCard" dans le laboratoire de test "TestPage".

--- star-wars-blog/src/pages/TestPage/TestPage.jsx
+++ star-wars-blog/src/pages/TestPage/TestPage.jsx
@@ -1,11 +1,12 @@
 import './TestPage.scss'
-import NotifSwitch from '../../components/NotifSwitch/NotifSwitch'
+
+
 
 export default function TestPage() {
 
   return (
     <div className='test-app'>
-      <NotifSwitch />
+      
     </div>
   )
 }
