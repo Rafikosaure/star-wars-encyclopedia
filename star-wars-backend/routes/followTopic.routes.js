ae2fc41057674c9f37c517ccf5c0ce7a03187fb3
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-11-09 17:14:13 +0100

    Retrait du middleware de vérification du login dans la route de récupération des discussions suivies par l'utilisateur : le système initial buggait au niveau du middleware.

--- star-wars-backend/routes/followTopic.routes.js
+++ star-wars-backend/routes/followTopic.routes.js
@@ -7,7 +7,7 @@ const router = express.Router()
 /** Routes de gestion des discussions suivies par les utilisateurs */
 router.post('/createAFollowTopicArray/:id', auth.verifyToken, followTopicCtrl.createAFollowTopicArray)
 router.get('/getAllFollowersOfATopic/:id', followTopicCtrl.getAllFollowersOfATopic)
-router.get('/getAllFollowedTopics', auth.verifyToken, followTopicCtrl.getAllFollowedTopics)
+router.get('/getAllFollowedTopics/:id', followTopicCtrl.getAllFollowedTopics)
 router.put('/chooseWhetherToFollowOrNot/:id', auth.verifyToken, followTopicCtrl.chooseWhetherToFollowOrNot)
 
 module.exports = router
