a0cc747eb1a2e06b8c1392461a1866c3c515897b
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-12-03 02:13:32 +0100

    Implémentation et mise en route de la fonctionnalité de pagination dans la page "Topic". Chaque page affiche un maximum de 10 posts, et il est possible de naviguer entre les pages tout en visualisant la page courante dans un compteur.

--- star-wars-blog/src/pages/Topic/Topic.jsx
+++ star-wars-blog/src/pages/Topic/Topic.jsx
@@ -12,6 +12,7 @@ import { selectIsLoggedState } from '../../redux/slices/isLoggedUserSlice'
 import { selectLoggedUser } from '../../redux/slices/loggedUserSlice'
 import { reloadPosts } from '../../redux/slices/postsReload'
 import { selectReloadPostsState } from '../../redux/slices/postsReload'
+import { selectTopicDozen, setCurrentTopicDozen } from '../../redux/slices/topicDozenSlice'
 import mentionsManager from '../../sharedFunctions/mentionsManager'
 import subscribersManager from '../../sharedFunctions/subscribersManager'
 import PostCard from '../../components/PostCard/PostCard'
@@ -19,18 +20,22 @@ import PostForm from '../../components/PostForm/PostForm'
 import { Link } from 'react-router-dom'
 import ReturnArrow from '../../assets/images/return-arrow.webp'
 import config from '../../config'
+import ArrowNext from '../../assets/images/next-arrow.webp'
+import ArrowPrev from '../../assets/images/back-arrow.webp'
 
 
 
 
 export default function Topic() {
     
-    const { topicId } = useParams()
+    const { topicId, page } = useParams()
     const [currentTopicData, setCurrentTopicData] = useState()
     const [currentCategory, setCurrentCategory] = useState()
     const [usersList, setUsersList] = useState()
     const [description, setDescription] = useState('')
     const [toReset, setToReset] = useState(false)
+    const [totalPages, setTotalPages] = useState(1);
+    const [currentPage, setCurrentPage] = useState(parseInt(page) || 1);
     const navigate = useNavigate()
     const { handleSubmit } = useForm()
     const currentCitation = useSelector(selectCitation)
@@ -38,25 +43,62 @@ export default function Topic() {
     const loggedUser = useSelector(selectLoggedUser)
     const isLogged = useSelector(selectIsLoggedState)
     const reloadUsers = useSelector(selectReloadUsersState)
+    const currentTopicDozen = useSelector(selectTopicDozen)
     const citationText = currentCitation.text
     const citationAuthorId = currentCitation.authorId
     const dispatch = useDispatch()
 
 
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [currentTopicDozen.value.currentPage])
+
+
     // Récupérer les posts de la discussion courante
     useEffect(() => {
-        fetch(`${config.serverEndpoint}/post/getPostsByTopicId/${topicId}`)
+        const currentTopicData = {
+            currentPage: page,
+            topicId: topicId
+        }
+        dispatch(setCurrentTopicDozen(currentTopicData))
+        fetch(`${config.serverEndpoint}/post/getPostsByTopicId/${topicId}/posts?page=${currentTopicDozen.value.currentPage}`)
         .then(response => response.json())
         .then(data => {
             if (data.error) {
                 navigate("*")
             } else {
                 setCurrentTopicData(data)
+                setTotalPages(data.totalPages)
+                setCurrentPage(data.currentPage)
             }
         })
-        .catch(error => console.log(error))
-    }, [navigate, topicId, reloadPostsBool])
+        .catch(error => {
+            console.log(error)
+            navigate("*")
+        })
+    }, [navigate, dispatch, topicId, page, currentPage, reloadPostsBool, currentTopicDozen])
 
+
+    // Vérifier si la page est valide
+    const isPageValid = (page, totalPages) => {
+        return page >= 1 && page <= totalPages;
+    };
+
+
+    useEffect(() => {
+
+        // Si la page est undefined ou invalide, on met pageNum à 1
+        const pageNum = parseInt(page) || 1; 
+        if (currentTopicData && totalPages && currentPage) {
+
+            // Vérification de la validité de la page
+            if (totalPages && !isPageValid(pageNum, totalPages)) {
+
+                // Rediriger vers la page d'erreur 404 si la page est invalide
+                navigate("*");
+            }
+        }
+    }, [page, totalPages, currentTopicData, currentPage, navigate]);
 
 
     // Récupérer la catégorie de la discussion courante et ses données
@@ -66,10 +108,12 @@ export default function Topic() {
         .then(data => {
             setCurrentCategory(data.category[0])
         })
-        .catch(error => console.log(error))
+        .catch(error => {
+            console.log(error)
+            navigate("*")
+        })
 
-    }, [topicId])
-
+    }, [topicId, navigate])
 
 
     useEffect(() => {
@@ -87,12 +131,46 @@ export default function Topic() {
             })
             .catch(error => {
                 console.log(error)
+                navigate("*")
             })
             dispatch(reloadUsersArrayFunction(true))
         }
         
-    }, [dispatch, reloadUsers, usersList])
+    }, [dispatch, navigate, reloadUsers, usersList])
 
+
+    // Fonction pour passer à la page suivante
+    const nextPage = (e) => {
+        e.preventDefault()
+        if (currentPage < totalPages) {
+            dispatch(reloadPosts())
+            const nextPage = currentPage + 1
+            setCurrentPage(nextPage);  // Incrémenter la page
+            const currentTopicData = {
+                currentPage: nextPage,
+                topicId: topicId
+            }
+            dispatch(setCurrentTopicDozen(currentTopicData)); // Mise à jour dans Redux
+            navigate(`/topic/${topicId}/page/${nextPage}`);  // Met à jour l'URL
+        }
+    };
+
+
+    // Fonction pour revenir à la page précédente
+    const prevPage = (e) => {
+        e.preventDefault()
+        if (currentPage > 1) {
+            dispatch(reloadPosts())
+            const prevPage = currentPage - 1;
+            setCurrentPage(prevPage);
+            const currentTopicData = {
+                currentPage: prevPage,
+                topicId: topicId
+            }
+            dispatch(setCurrentTopicDozen(currentTopicData)); // Mise à jour dans Redux
+            navigate(`/topic/${topicId}/page/${prevPage}`);  // Met à jour l'URL     
+        }
+    };
 
 
     const createNewPost = (data) => {
@@ -132,10 +210,10 @@ export default function Topic() {
 
             // Gestion des mentions
             dispatch(reloadUsersArrayFunction(false))
-            mentionsManager(data.description, result.newPost._id, usersList, topicId)
+            mentionsManager(data.description, result.newPost._id, usersList, topicId, currentTopicDozen.value.currentPage)
             
             // Gestion des abonnements à la discussion
-            subscribersManager(topicId, result.newPost._id, loggedUser, "post")
+            subscribersManager(topicId, result.newPost._id, loggedUser, "post", currentTopicDozen.value.currentPage)
 
             // Rafraichissement des posts affichés
             dispatch(reloadPosts())
@@ -161,11 +239,36 @@ export default function Topic() {
                 <div className='topic-content'>
                     <h1 className='topic-title'>{currentTopicData.title}</h1>
                     {currentTopicData && (
+                    <>
                     <div className='topic-list'>
-                        {currentTopicData.posts.map((post, index) => (
-                            <PostCard key={index} index={index} post={post} topicId={topicId} usersList={usersList} />
-                        ))}
+                        {currentTopicData.posts.map((post, index) => {
+                            // Calcul de l'index global
+                            const globalIndex = (currentPage - 1) * 10 + index + 1;
+                        return (
+                            <PostCard key={index} index={globalIndex} post={post} topicId={topicId} usersList={usersList} currentPage={currentTopicDozen.value.currentPage} />
+                        )})}
                     </div>
+                    {totalPages > 1 && (
+                        <div className='topic-page-buttons'>
+                            {currentPage !== 1 && (
+                                <div className='topic-prev-button' onClick={(e) => prevPage(e)} 
+                                title='Page précédente'
+                                ><img src={ArrowPrev} className='topic-button-prev-page' alt="Bouton de retour à la page précédente" />
+                                </div>
+                            )}
+                            
+                            <span className='topic-page-counter'>Page {currentPage} sur {totalPages}</span>
+
+                            {currentPage !== totalPages && (
+                                <div className='topic-next-button' onClick={(e) => nextPage(e)} 
+                                title='Page suivante'
+                                ><img src={ArrowNext} className='topic-button-next-page' alt="Bouton de passage à la page suivante" />
+                                </div>
+                            )}
+                        </div>
+                    )}
+                    
+                    </>
                     )}
                     {isLogged && (
                         <form id='citation-post' className='creation-post-form' onSubmit={handleSubmit(createNewPost)}>
