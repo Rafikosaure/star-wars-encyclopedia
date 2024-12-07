9278935055972cb0f28ccdab4568e56a9cac1090
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-12-07 14:11:18 +0100

    debug images du site pour le déploiement (part 11)

--- star-wars-backend/controllers/user.controller.js
+++ star-wars-backend/controllers/user.controller.js
@@ -22,6 +22,8 @@ exports.modifyUser = async (req, res) => {
         const ref = `${name}-${timestamp}.webp`
         const path = `images/${ref}`
         sharp(buffer).resize(450).webp().toFile(path)
+        console.log('TEST :', req.file.path)
+        // fs.unlink(req.file.path, () => { });
         // profilePicture = `${req.protocol}://${req.get('host')}/images/${ref}`
         profilePicture = `${ENV.DEPLOYED_EXPRESS_SERVER_ENDPOINT}/images/${ref}`
     }
