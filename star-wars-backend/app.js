73104c923dfcf5c5b19404a73766271c9f04dc38
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-12-07 12:07:51 +0100

    debug images du site pour le déploiement (part 3)

--- star-wars-backend/app.js
+++ star-wars-backend/app.js
@@ -73,7 +73,7 @@ app.use('/followTopic', followTopicRoutes)
 app.use('/like', likeRoutes)
 app.use('/email', emailRoutes)
 app.use('/isMentionned', isMentionnedRoutes)
-app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use('/images', cors(), express.static(path.join(__dirname, 'images')))
 
 
 module.exports = app
