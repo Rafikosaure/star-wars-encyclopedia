ae02357b1be6c9b3756a1e4933da6760be31cd7a
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-12-07 12:05:26 +0100

    debug images du site pour le déploiement (part 3)

--- star-wars-backend/app.js
+++ star-wars-backend/app.js
@@ -20,7 +20,7 @@ mongoose
 
 // Gestion des erreurs CORS
 app.use(cors({
-        origin: [ENV.CORS_ORIGIN, ENV.CORS_IMAGES],
+        origin: ENV.CORS_ORIGIN,
         credentials: true,
         methods: ['GET', 'PUT', 'POST', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization']
