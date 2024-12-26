adeb623d5f7769d35647478985607e8401c2ffd1
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-12-26 14:12:01 +0100

    Mise à jour des largeurs des pages, du Header et du Footer, et refonte légère de la mise en page de la page d'Accueil, pour une meilleure gestion du responsive design.

--- star-wars-blog/src/pages/Home/Home.jsx
+++ star-wars-blog/src/pages/Home/Home.jsx
@@ -11,22 +11,20 @@ export default function Home() {
   const homeText = "Bienvenue dans notre encyclopédie galactique dédiée à Star Wars ! Plongez dans un univers où la Force guide chaque page et où les légendes prennent vie. Que vous soyez un jedi expérimenté ou un padawan curieux, explorez avec nous l’histoire de la saga et découvrez chaque détail de son univers iconique. Que la connaissance soit avec vous dans cette aventure interstellaire !"
 
   return (
-    <div className='app'>
-      <div className='home-page'>
-        <div className='div-welcome'>
-          <p className='welcome-message'>
-            {homeText}
-          </p>
-        </div>
-        <div className='div-categories'>
-          <div className='transition1' />
-          <div className='transition2' />
-          <h2>catégories</h2>
-          <div className='card-list'>
-            {data.map((item) => 
-                <Card key={item._id} item={item} />
-            )}
-          </div>
+    <div className='app home-page'>
+      <div className='div-welcome'>
+        <p className='welcome-message'>
+          {homeText}
+        </p>
+      </div>
+      <div className='div-categories'>
+        <div className='transition1' />
+        <div className='transition2' />
+        <h2>catégories</h2>
+        <div className='card-list'>
+          {data.map((item) => 
+              <Card key={item._id} item={item} />
+          )}
         </div>
       </div>
     </div>
