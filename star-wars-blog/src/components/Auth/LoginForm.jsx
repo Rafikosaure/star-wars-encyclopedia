4d4c8237cd8cdf907ef3ec463d77afa38d8697a2
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2025-02-22 19:28:51 +0100

    Ajout d'un commentaire pour clarifier.

--- star-wars-blog/src/components/Auth/LoginForm.jsx
+++ star-wars-blog/src/components/Auth/LoginForm.jsx
@@ -18,6 +18,7 @@ export default function LoginForm() {
     const title = 'Connexion'
 
 
+    // Connexion de l'utilisateur
     const onSubmit = (data) => {
         fetch(`${config.serverEndpoint}/auth/login`, {
             method: "POST",
