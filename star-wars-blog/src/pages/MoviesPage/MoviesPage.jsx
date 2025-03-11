d1f09154c3286104f7c1917d5b7675e13719aeaf
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2025-03-11 19:30:36 +0100

    Création de la page "MoviesPage" et de ses styles de base.

---
+++ star-wars-blog/src/pages/MoviesPage/MoviesPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './MoviesPage.scss'
+import '../../sharedStyles/index.scss'
+
+
+function MoviesPage() {
+    return (
+        <div className='app movies-page-wrapper'>
+            <h2>Films</h2>
+            <div className='movies-page-content'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam voluptate quam ipsam. Temporibus velit, nihil autem odio libero vitae maxime dolore debitis atque ullam eligendi aut cum iure, aspernatur earum veniam aperiam voluptatem eaque. Repellendus fugit nihil omnis ipsam. Quasi et possimus voluptatum ab consequuntur sed dolor voluptates hic ut, aliquam exercitationem consequatur magnam quos incidunt, pariatur quisquam quibusdam repudiandae aspernatur id reprehenderit. Quod iste pariatur, voluptate reprehenderit, assumenda architecto illo corporis dolorum, unde repellendus ratione accusamus cumque harum minus autem at iure! Ex consequatur error dolor. Quidem ipsa itaque, obcaecati illo ullam in, rerum veniam similique rem amet natus minima illum. Quidem aut nisi deserunt eius. Ut, modi fugit eveniet ipsum cupiditate praesentium cum accusamus corporis voluptates deleniti. Nihil, rerum necessitatibus. Impedit velit atque officia nisi ad facere nesciunt.</div>
+        </div>
+    )
+}
+
+export default MoviesPage
