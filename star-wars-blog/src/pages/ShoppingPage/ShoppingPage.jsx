523f7d39bf8cb77fc86e410d41d3bdf8d8eaeb5f
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2025-03-11 19:31:01 +0100

    Création de la page "ShoppingPage" et de ses styles de base.

---
+++ star-wars-blog/src/pages/ShoppingPage/ShoppingPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './ShoppingPage.scss'
+import '../../sharedStyles/index.scss'
+
+
+function ShoppingPage() {
+    return (
+        <div className='app shopping-page-wrapper'>
+            <h2>Boutique en ligne</h2>
+            <div className='shopping-page-content'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati eos maxime necessitatibus deleniti possimus dignissimos inventore sapiente corrupti nihil ipsam quia veniam numquam, eius debitis architecto, tempore sint perspiciatis! Accusantium, cumque doloribus omnis quidem consequuntur laborum eaque perspiciatis earum corporis ullam facilis assumenda soluta cum dolores nobis nesciunt adipisci delectus quos culpa et neque? Expedita incidunt ipsa libero consequuntur non est alias enim fugit. Rem consequuntur qui, quibusdam, sint tempore necessitatibus tempora fugit at laudantium nam magnam repudiandae consectetur laborum inventore possimus magni sequi vel officia dolorum excepturi, accusantium exercitationem deleniti? Laudantium consectetur deserunt doloremque dolorem fugiat odio voluptatem quidem, quisquam fugit, repudiandae quaerat aspernatur earum. Facere eos nisi porro quasi, laudantium vel expedita quisquam, laborum aperiam atque dolor, illum aspernatur delectus. Ut aliquid hic iusto magni, mollitia architecto sequi veritatis assumenda? Fugiat, suscipit! Dolorum, vitae, aut nihil modi aliquam nemo dicta impedit consequatur magni amet doloribus facilis facere quae.
+            </div>
+        </div>
+    )
+}
+
+export default ShoppingPage
