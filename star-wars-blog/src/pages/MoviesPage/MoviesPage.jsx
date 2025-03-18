ccf81a2c56f8b511ded940bcb987b54583a15add
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2025-03-18 12:42:01 +0100

    Intégration de la page "MoviesPage" (vidéothèque du site) et de son fonctionnement.

--- star-wars-blog/src/pages/MoviesPage/MoviesPage.jsx
+++ star-wars-blog/src/pages/MoviesPage/MoviesPage.jsx
@@ -1,13 +1,251 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './MoviesPage.scss'
 import '../../sharedStyles/index.scss'
+import BackArrow from '../../assets/images/back-arrow.webp'
+import NextArrow from '../../assets/images/next-arrow.webp'
+import { TmdbApiServices } from '../../api/api-tmdb'
+import config from '../../config'
+import DisneyPlusMoviesData from '../../data/moviesDisneyPlusURLs.json'
+import TrailerModal from '../../components/TrailerModal/TrailerModal'
+
 
 
 function MoviesPage() {
+
+    const [listingIsDisplayed, setListingIsDisplayed] = useState('listing-open')
+    const [listingItemsAreDisplayed, setListingItemsAreDisplayed] = useState('listing-content-open')
+    const [alternateTextPosition, setAlternateTextPosition] = useState('alternate-text-to-right')
+    const [listingDisplayArrow, setListingDisplayArrow] = useState(BackArrow)
+    const [tmdbMoviesData, setTmdbMoviesData] = useState()
+    const [tmdbSeriesData, setTmdbSeriesData] = useState()
+    const [moviesList, setMoviesList] = useState([])
+    const [mediaType, setMediaType] = useState('films')
+    const [isActiveMoviesColor, setIsActiveMoviesColor] = useState('isActiveMovies')
+    const [isActiveSeriesColor, setIsActiveSeriesColor] = useState('')
+    const [currentMediaId, setCurrentMediaId] = useState()
+    const [currentMovie, setCurrentMovie] = useState()
+    const [currentMovieURL, setCurrentMovieURL] = useState()
+    const [currentMovieCreators, setCurrentMovieCreators] = useState()
+    const [currentMovieActors, setCurrentMovieActors] = useState()
+    const [currentMovieTrailerURL, setCurrentMovieTrailerURL] = useState()
+    const [datetime, setDatetime] = useState(new Date())
+    const { fetchStarWarsMovies } = TmdbApiServices
+    
+
+    // Récupération des films Star Wars
+    useEffect(() => {
+        fetchStarWarsMovies(1, config.tmdbMoviesListId)
+        .then(res => setTmdbMoviesData(res.items))
+        .catch(err => console.error(err));
+    }, [fetchStarWarsMovies])
+
+
+    // Récupération des séries Star Wars
+    useEffect(() => {
+        fetchStarWarsMovies(1, config.tmdbSeriesListId)
+        .then(res => setTmdbSeriesData(res.items))
+        .catch(err => console.error(err));
+    }, [fetchStarWarsMovies])
+
+
+    // Tri des films et séries par date de sortie 
+    // + affichage des films ou des séries
+    useEffect(() => {
+        if (mediaType === 'films' && tmdbMoviesData) {
+            setMoviesList(tmdbMoviesData.sort((a, b) => a.release_date < b.release_date))
+            setIsActiveMoviesColor('isActiveMovies')
+            setIsActiveSeriesColor('')
+        } else if (mediaType === 'series' && tmdbSeriesData) {
+            setMoviesList(tmdbSeriesData.sort((a, b) => a.first_air_date < b.first_air_date))
+            setIsActiveSeriesColor('isActiveSeries')
+            setIsActiveMoviesColor('')
+        }
+    }, [mediaType, isActiveMoviesColor, isActiveSeriesColor, tmdbMoviesData, tmdbSeriesData])
+
+
+    // Récupération des données du film / de la série sélectionné-ée
+    useEffect(() => {
+        if (currentMediaId && tmdbMoviesData && mediaType === 'films') {
+            const movie = tmdbMoviesData.filter(movie => movie.id === currentMediaId)
+            setCurrentMovie(movie[0])
+            setDatetime(new Date(movie[0].release_date))
+        } else if (currentMediaId && tmdbSeriesData && mediaType === 'series') {
+            const movie = tmdbSeriesData.filter(movie => movie.id === currentMediaId)
+            setCurrentMovie(movie[0])
+            setDatetime(new Date(movie[0].first_air_date))
+        }
+    }, [currentMediaId, mediaType, tmdbMoviesData, tmdbSeriesData])
+
+
+    // Récupération des informations locales du média courant
+    useEffect(() => {
+        if (currentMediaId) {
+            const disneyPlusObject = DisneyPlusMoviesData.find(movie => movie.id === currentMediaId)
+            if (disneyPlusObject) {
+                setCurrentMovieURL(disneyPlusObject.disneyPlusUrl)
+                setCurrentMovieCreators(disneyPlusObject.realisedBy || disneyPlusObject.createdBy)
+                setCurrentMovieActors(disneyPlusObject.actors)
+                setCurrentMovieTrailerURL(disneyPlusObject.trailerURL)
+            }
+        }
+    }, [currentMediaId])
+
+    
+    // Fonction par défaut pour ouvrir ou fermer la liste des médias
+    const displayListingFunction = (e) => {
+        e.preventDefault()
+        if (listingIsDisplayed === 'listing-open') {
+            setListingItemsAreDisplayed('listing-content-close')
+            setListingIsDisplayed('listing-close')
+            setListingDisplayArrow(NextArrow)
+            setAlternateTextPosition('alternate-text-to-center')
+        } else {
+            setListingDisplayArrow(BackArrow)
+            setListingItemsAreDisplayed('listing-content-open')
+            setListingIsDisplayed('listing-open')
+            setAlternateTextPosition('alternate-text-to-right')
+        }
+    }
+
+
+    // fonction pour ouvrir la liste des médias depuis le menu
+    const openListingFunction = (e) => {
+        e.preventDefault()
+        if (listingIsDisplayed === 'listing-close') {
+            setListingDisplayArrow(BackArrow)
+            setListingItemsAreDisplayed('listing-content-open')
+            setListingIsDisplayed('listing-open')
+            setAlternateTextPosition('alternate-text-to-right')
+        }
+    }
+
+
     return (
         <div className='app movies-page-wrapper'>
-            <h2>Films</h2>
-            <div className='movies-page-content'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam voluptate quam ipsam. Temporibus velit, nihil autem odio libero vitae maxime dolore debitis atque ullam eligendi aut cum iure, aspernatur earum veniam aperiam voluptatem eaque. Repellendus fugit nihil omnis ipsam. Quasi et possimus voluptatum ab consequuntur sed dolor voluptates hic ut, aliquam exercitationem consequatur magnam quos incidunt, pariatur quisquam quibusdam repudiandae aspernatur id reprehenderit. Quod iste pariatur, voluptate reprehenderit, assumenda architecto illo corporis dolorum, unde repellendus ratione accusamus cumque harum minus autem at iure! Ex consequatur error dolor. Quidem ipsa itaque, obcaecati illo ullam in, rerum veniam similique rem amet natus minima illum. Quidem aut nisi deserunt eius. Ut, modi fugit eveniet ipsum cupiditate praesentium cum accusamus corporis voluptates deleniti. Nihil, rerum necessitatibus. Impedit velit atque officia nisi ad facere nesciunt.</div>
+            <div className='movies-media-navbar'>
+                <div className={`div-movies ${isActiveMoviesColor}`} onClick={(e) => {setMediaType('films'); setCurrentMediaId(); setCurrentMovie(); openListingFunction(e)}}>films</div>
+                <div className={`div-series ${isActiveSeriesColor}`} onClick={(e) => {setMediaType('series'); setCurrentMediaId(); setCurrentMovie(); openListingFunction(e)}}>séries</div>
+            </div>
+            <h1 className='movies-page-title'>Vidéothèque</h1>
+            <div className='movies-page-content'>
+                <aside className={`movies-page-media-listing ${listingIsDisplayed}`}>
+                    <div className='movies-page-media-listing-image-wrapper'>
+                        <img src={listingDisplayArrow} alt="Ouvrir ou fermer la liste des médias" 
+                        onClick={(e) => displayListingFunction(e)}
+                        title={listingDisplayArrow === BackArrow ? (
+                            'Fermer le menu'
+                        ) : ('Ouvrir le menu')}
+                        />
+                    </div>
+                    <ul className={`movies-page-media-listing-content ${listingItemsAreDisplayed}`}>
+                        {moviesList.length > 0 && 
+                            moviesList.map((movie) => (
+                                mediaType === 'films' ? (
+                                <li 
+                                key={movie.id}
+                                className={`media-listing-title`}
+                                onClick={(e) => {setCurrentMediaId(movie.id); displayListingFunction(e)}}
+                                style={{ color: movie.id === currentMediaId && 'rgb(53, 155, 155)' }}
+                                >{movie.title}</li>
+                            ) : (
+                                <li
+                                key={movie.id}
+                                className={`media-listing-title`}
+                                onClick={(e) => {setCurrentMediaId(movie.id); displayListingFunction(e)}}
+                                style={{ color: movie.id === currentMediaId && 'rgb(53, 155, 155)' }}
+                                >{movie.name}</li>
+                            ))
+                        )}
+                    </ul>
+                    <div className='movies-page-media-listing-image-wrapper movies-page-media-listing-image-bottom'>
+                        <img src={listingDisplayArrow} alt="Ouvrir ou fermer la liste des médias" 
+                        onClick={(e) => displayListingFunction(e)}
+                        title={listingDisplayArrow === BackArrow ? (
+                            'Fermer le menu'
+                        ) : ('Ouvrir le menu')}
+                        />
+                    </div>
+                </aside>
+                {currentMovie ? (
+                <section className={`movies-page-data-section`}>
+                    <div className='movies-page-data-section-text-wrapper'>
+                        {mediaType === 'films' ? (
+                            <h2 className='movies-page-data-section-title'>{currentMovie.title}</h2>
+                        ) : (
+                            <h2 className='movies-page-data-section-title'>{currentMovie.name}</h2>
+                        )}
+                        <p className='movies-page-data-section-release-date'>
+                            {`
+                            Date de sortie : 
+                            ${datetime.toLocaleDateString(
+                                "fr-FR", 
+                                {year: "numeric", month: "long", day: "numeric"}
+                            )}
+                            `}
+                        </p>
+                        <p className='movies-page-data-section-text-content'>{currentMovie.overview}</p>
+                        {currentMovieTrailerURL !== "" && (
+                            <div className='movies-page-data-section-trailer'>
+                                <TrailerModal videoURL={currentMovieTrailerURL} />
+                            </div>
+                        )}
+                        {currentMovieURL && (
+                            mediaType === "films" ? (
+                                <a 
+                                className='movies-page-data-section-link'
+                                href={currentMovieURL} 
+                                target='_blank'
+                                rel="noopener noreferrer"
+                                >Accéder au film sur Disney +
+                                </a>
+                            ) : (
+                                <a 
+                                className='movies-page-data-section-link'
+                                href={currentMovieURL} 
+                                target='_blank'
+                                rel="noopener noreferrer"
+                                >Accéder à la série sur Disney +
+                                </a>
+                            )
+                        )}
+                    </div>
+                    <div className='movies-page-data-section-image-actors-creators-wrapper'>
+                        <div className='movies-page-data-section-image-wrapper'>
+                            <img className='movie-page-data-section-image' src={`${config.tmdbMovieImagePath}${currentMovie.poster_path}`} alt="Poster du film / de la série" />
+                        </div>
+                        <section className='movies-page-data-section-actors-creators'>
+                            {mediaType === 'films' ? (
+                                <h3 className='movies-page-data-section-actors-creators-title'>Réalisé par :</h3>
+
+                            ) : (
+                                <h3 className='movies-page-data-section-actors-creators-title'>Créé par :</h3>
+                            )}
+                            {currentMovieCreators && (
+                                currentMovieCreators.map((creator, index) => (
+                                    <p className='movies-page-data-section-crew' key={index}>{creator}</p>
+                                ))
+                            )}
+                            <h3 className='movies-page-data-section-actors-creators-title actors-section'>Avec :</h3>
+                            {currentMovieActors && (
+                                currentMovieActors.map((actor, index) => (
+                                    <p className='movies-page-data-section-crew' key={index}>{actor}</p>
+                                ))
+                            )}
+                        </section>
+                        
+                    </div>
+                    
+                </section>
+                ) : (
+                <div className='movies-page-data-section-alternate-text-section'>
+                    {mediaType === 'films' ? (
+                        <h3 className={`movies-page-data-section-alternate-text ${alternateTextPosition}`}>Sélectionnez un film dans le menu à votre gauche</h3>
+                    ) : (
+                        <h3 className={`movies-page-data-section-alternate-text ${alternateTextPosition}`}>Sélectionnez une série dans le menu à votre gauche</h3>
+                    )}
+                </div>
+                )}
+            </div>            
         </div>
     )
 }
