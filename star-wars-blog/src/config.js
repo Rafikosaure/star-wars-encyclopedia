51cb69fee55717ce67d6ff7909bf4917ef248d14
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2025-03-18 12:38:13 +0100

    Mise en place des variables d'environnement nécessaires pour la vidéothèque (page "MoviesPage").

--- star-wars-blog/src/config.js
+++ star-wars-blog/src/config.js
@@ -2,7 +2,12 @@ const config = {
     deeplApiKey: process.env.REACT_APP_DEEPL_API_KEY,
     serverEndpoint: process.env.REACT_APP_EXPRESS_SERVER_ENDPOINT,
     starWarsAPI: process.env.REACT_APP_STAR_WARS_DATABANK_API,
-    persistentConnect: process.env.REACT_APP_PERSISTENT_CONNECTION_TOKEN
+    persistentConnect: process.env.REACT_APP_PERSISTENT_CONNECTION_TOKEN,
+    tmdbApiKey: process.env.REACT_APP_TMDB_API_KEY,
+    tmdbMoviesListId: process.env.REACT_APP_TMDB_MOVIES_LIST_ID,
+    tmdbSeriesListId: process.env.REACT_APP_TMDB_SERIES_LIST_ID,
+    tmdbMovieImagePath: process.env.REACT_APP_TMDB_MOVIE_IMAGE_PATH,
+    tmdbApiEndpoint: process.env.REACT_APP_TMDB_API_ENDPOINT
 }
 
 export default config
