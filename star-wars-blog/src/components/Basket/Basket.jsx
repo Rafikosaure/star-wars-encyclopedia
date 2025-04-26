e262d8fa4745b0f9ee41dbd9d0dc17a30775c8ec
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2025-04-26 13:38:33 +0200

    Retravail sur le composant "Panier": mise en place des liens de redirection vers la page "Panier" et de vidage du panier dans Redux. Egalement : gestion des styles du composants.

--- star-wars-blog/src/components/Basket/Basket.jsx
+++ star-wars-blog/src/components/Basket/Basket.jsx
@@ -6,13 +6,23 @@ import { emptyBasket, removeProduct } from '../../redux/slices/shoppingBasket'
 import { ReactComponent as EmptyBasket } from '../../assets/images/shopping-basket-empty-white.svg'
 import { ReactComponent as FullBasket } from '../../assets/images/shopping-basket-full-white.svg'
 import { toast } from 'sonner'
+import { useNavigate } from 'react-router-dom'
 
 
 function Basket() {
 
     const basketContent = useSelector(selectBasket)
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
+
+    // Se rendre sur la page de panier
+    const goToBasket = (e) => {
+        e.preventDefault()
+        if (basketContent.length !== 0) {
+            navigate("/basket")
+        }
+    }
 
     // Vider le panier
     const emptyBasketFunction = (e) => {
@@ -44,16 +54,19 @@ function Basket() {
 
     return (
         <div className='basket-wrapper'>
-            <span className='basket-title-all'>
+            <span className='basket-title-all'
+            >
                 {basketContent.length === 0 ? (
                     <div
                     title='Le panier est vide'
                     ><EmptyBasket /></div>
                 ) : (
                     <div 
-                    onClick={(e) => emptyBasketFunction(e)}
-                    title='Cliquez pour vider le panier'
+                    onClick={(e) => goToBasket(e)}
+                    onKeyDown={(e) => e.key === 'Enter' && goToBasket(e)}
+                    title='Aller au panier'
                     className='basket-title-full-basket'
+                    tabIndex="0"
                     ><FullBasket /></div>
                 )}
                 <h1 className='basket-title'>Nombre d'articles : {basketContent.length}</h1>
@@ -62,6 +75,25 @@ function Basket() {
             <div className='basket-content'>
                 {basketContent.length !== 0 ? (
                     <>
+                    <span className='basket-content-manager-section'>
+                        <p 
+                        className='basket-empty-button'
+                        onClick={(e) => emptyBasketFunction(e)}
+                        onKeyDown={(e) => e.key === 'Enter' && emptyBasketFunction(e)}
+                        title='Vider le panier'
+                        tabIndex="0"
+                        >⨠ Vider le panier</p>
+
+                        <p 
+                        className='basket-page-link'
+                        onClick={(e) => goToBasket(e)}
+                        onKeyDown={(e) => e.key === 'Enter' && goToBasket(e)}
+                        title='Visiter le panier'
+                        tabIndex="0"
+                        >⨠ Voir panier</p>
+                    </span>
+                    
+
                     <p className='basket-product'><strong>Contenu du panier :</strong></p>
                     {groupedBasket.map((product, index) => (
                         
@@ -73,7 +105,9 @@ function Basket() {
                             <p 
                                 title='Retirer le produit du panier'
                                 onClick={(e) => removeProductFunction(e, product)}
+                                onKeyDown={(e) => e.key === 'Enter' && removeProductFunction(e, product)}
                                 className='basket-product-remove-cross'
+                                tabIndex="0"
                             >✖</p>
                         </span> 
                     ))}
