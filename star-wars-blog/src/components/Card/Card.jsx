43dd43d45ee68db3c81bada9b6559202a071ab7a
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2025-06-15 05:08:04 +0200

    Simplification du code du composant Card : déchargement de la responsabilité de traduire le nom de l'article avant de l'afficher (un appel à Deepl en moins à charger dans chaque carte).

--- star-wars-blog/src/components/Card/Card.jsx
+++ star-wars-blog/src/components/Card/Card.jsx
@@ -1,9 +1,7 @@
-import { useState, useEffect } from 'react'
 import './Card.scss'
 import { Link, useLocation } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { reinitializeDozen } from '../../redux/slices/dozenSlice'
-import { ServerServices } from '../../api/api-server'
 import '../../theme/index.scss'
 
 
@@ -14,36 +12,20 @@ export default function Card({ item, categoryId }) {
   const location = useLocation()
   const itemId = item._id
   const paramsIds = `${categoryId}.${itemId}`
-  const [translatedName, setTranslatedName] = useState()
-  const { translateText } = ServerServices
-
-  
-  // Traduction linguistique automatique du sujet traité
-  useEffect(() => {
-    if (location.pathname !== "/") {
-      const fetchTranslation = async () => {
-        const translation = await translateText("EN", "FR", item.name);
-        if (translation) {
-          setTranslatedName(translation);
-        }
-      };
-      fetchTranslation();
-    }
-    }, [item.name, location, translateText])
     
 
   return (
     <>
       {location.pathname !== "/" ? (
-        item && item.name && item.image && translatedName && (
+        item && item.name && item.image && (
         <Link className='card card-appears-animation' to={`/article/${paramsIds}`}>
           <div className='card-content'>
             <div className='card-image'>
               <img src={item.image} loading='lazy' alt={`Carte de l'article "${item.name}"`} />
             </div>
             <div className='card-name-div'>
-              {translatedName && (
-                <p className="card-name">{translatedName}</p>
+              {item && (
+                <p className="card-name">{item.name}</p>
               )}
             </div>
           </div>
