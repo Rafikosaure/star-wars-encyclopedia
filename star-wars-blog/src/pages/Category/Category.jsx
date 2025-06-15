1fdf1423e2c6767b146d6cd0f4f78a3deb2b79d3
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2025-06-15 05:10:45 +0200

    Optimisations : prise en charge de la traduction de tous les noms d'articles dans un seul appel au serveur Node (puis vers Deepl), 10 par 10 articles, et également : retrait du composant CardObserverWrapper pour des raisons de performances dans l'affichage des cartes.

--- star-wars-blog/src/pages/Category/Category.jsx
+++ star-wars-blog/src/pages/Category/Category.jsx
@@ -1,7 +1,6 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState, useCallback } from 'react'
 import './Category.scss'
 import Card from '../../components/Card/Card'
-import CardObserverWrapper from '../../components/CardObserverWrapper/CardObserverWrapper'
 import SearchBar from '../../components/SearchBar/SearchBar'
 import data from '../../data/localApiCategories.json'
 import NextArrow from '../../assets/images/next-arrow.webp'
@@ -14,6 +13,7 @@ import { reinitializeDozen, maxDozen, nextDozen, prevDozen, selectDozen } from '
 import { selectArticle } from '../../redux/slices/articleSlice'
 import { memorizeLastCategoryId } from '../../redux/slices/lastCategory'
 import { StarWarsApiServices } from '../../api/api-sw'
+import { ServerServices } from '../../api/api-server'
 import WikiNavbar from '../../components/WikiNavbar/WikiNavbar'
 
 
@@ -24,17 +24,33 @@ export default function Category() {
   const storedDozen = useSelector(selectDozen)
   const article = useSelector(selectArticle)
   const { categoryId } = useParams()
-  const [items, setItems] = useState([])
+  const [translatedItems, setTranslatedItems] = useState([])
   const [info, setInfo] = useState([])
   const [rightPage, updateRightPage] = useState(false)
   const [spinnerDisplay, setSpinnerDisplay] = useState('none')
   const nbDozen = useRef()
   const navigate = useNavigate()
   const { fetchStarWarsCategoryData } = StarWarsApiServices
+  const { translateText } = ServerServices
   
 
   // Trouver les articles correspondant à la catégorie choisie
   const currentDatas = data.find((item) => item._id === categoryId)
+    
+
+  // Traduire les noms des articles de manière groupée
+  // pour éviter de faire une requête par article
+  const translateTextCallback = useCallback(async (data) => {
+    try {
+      const frenchNames = await translateText("EN", "FR", data.map(item => item.name).join(','));
+      setTranslatedItems(data.map((item, index) => ({
+        ...item,
+        name: frenchNames.split(',')[index] || item.name
+      })));
+    } catch (error) {
+      console.error("Error translating article names:", error);
+    }
+  }, [translateText]);
 
 
   // Récupérer les informations depuis l'API
@@ -53,14 +69,14 @@ export default function Category() {
       .then(data => {
         if (data) {
           setInfo(data.info);
-          setItems(data.data);
+          translateTextCallback(data.data);
         }
         setSpinnerDisplay("none");
       });
       nbDozen.current = Math.ceil(info.total / 10);
     }
-  }, [categoryId, currentDatas, navigate, storedDozen, info.total, fetchStarWarsCategoryData, dispatch])
-  
+  }, [categoryId, currentDatas, navigate, storedDozen, info.total, fetchStarWarsCategoryData, dispatch, translateTextCallback]);
+
 
   // Gestion du clic sur le bouton 
   // pour voir les articles suivants
@@ -98,8 +114,8 @@ export default function Category() {
               <img className='card-list-spinner spinner-3' style={{display: `${spinnerDisplay}`}} src={Spinner} alt="Troisième spinner de chargement" />
               {article.value === undefined ? 
                 (
-                  items.map((item) =>
-                    <CardObserverWrapper key={item._id} item={item} categoryId={categoryId} />
+                  translatedItems.map((item) =>
+                    <Card key={item._id} item={item} categoryId={categoryId} />
                   )
                 ) : (
                     <Card key={article._id} item={article.value} categoryId={categoryId} />
