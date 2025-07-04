4138fa575f9715afb9e44241220ce5fdb61484df
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2025-07-04 22:08:18 +0200

    Adaptation de la fonction de récupération des données de la partie "shopping": les données proviennent désormais de l'API (projet Nextjs).

--- star-wars-blog/src/api/api-shopping.js
+++ star-wars-blog/src/api/api-shopping.js
@@ -1,27 +1,17 @@
 import config from '../config'
 
-// SWITCH DES DONNEES DU SHOPPING :
-// const localOrigin = true -> données locales
-// const localOrigin = false -> données de l'API
-const localOrigin = false;
 
-
-// Appels centralisés vers le serveur Node.JS du projet
+// Appels centralisés vers l'API des artefacts Star Wars
 export const ShoppingServices = {
 
     // Récupérer les produits de l'API (boutique)
     async getProductsToSell() {
-        if (localOrigin) {
-            const shoppingData = await import('../data/shoppingData.json')
-            return shoppingData.default;
-        } else {
-            try {
-                const productsArray = await fetch(`${config.starWarsShoppingApi}?limit=all`);
-                return productsArray.json()
-            } catch(error) {
-                console.log(error)
-                return []
-            }
+        try {
+            const productsArray = await fetch(`${config.starWarsShoppingApi}?limit=all`);
+            return productsArray.json()
+        } catch(error) {
+            console.log(error)
+            return []
         }
     }
 };
